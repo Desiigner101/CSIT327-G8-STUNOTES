7c116d6ab60c08b069d8c5c6c38953282dd5f449
Kervin Gino Sarsonas <Desiigner4074@gmail.com>
2025-09-28 22:02:45 +0800

    chore: changed launch path to start in registration page

--- stunotesapp/urls.py
+++ stunotesapp/urls.py
@@ -6,7 +6,7 @@ from notes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
+    path('', views.register_view, name='register'),
     path('', include('notes.urls')),
     #path('test/', include('stunotes.urls')),  
     
