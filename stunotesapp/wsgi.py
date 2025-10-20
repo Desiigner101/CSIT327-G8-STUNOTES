7b496839fc6976ed1355c5daabec14bdaca07923
Kervin Gino Sarsonas <Desiigner4074@gmail.com>
2025-10-20 19:07:16 +0800

    chore: Configure for Vercel deployment

--- stunotesapp/wsgi.py
+++ stunotesapp/wsgi.py
@@ -13,4 +13,6 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'stunotesapp.settings')
 
 application = get_wsgi_application()
-app = application  # ADD THIS LINE - Vercel needs it!
+
+# Vercel needs this
+app = application
