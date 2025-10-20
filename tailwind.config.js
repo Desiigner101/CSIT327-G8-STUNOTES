3142fc3654feb54e206552315ceede355183f801
Kervin Gino Sarsonas <Desiigner4074@gmail.com>
2025-10-20 16:08:34 +0800

    Apply .gitignore and remove tracked build artifacts

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './templates/**/*.html',
+    './notes/templates/**/*.html',
+    './view/templates/**/*.html',
+  ],
+}
